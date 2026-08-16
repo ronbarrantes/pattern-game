3a46c895989fd14dbf9d011a1cccb17163bc7a23
ronbarrantes <ronbarrantes@gmail.com>
2026-08-16 16:33:36 -0400

    Add rules of the game

--- pattern-game.c
+++ pattern-game.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+/** IO LOGIC **/
+
+/** GAME LOGIC **/
+// game starts
+// start sequence (lights up the LEDs 3 TIMES)
+//
+// there is sequence that starts with 3 colors
+// then 4 colors, then 5 colors and so on and so forth
+//
+// maybe I can have up to 32
+// if player loses then it displays a "sad" sequece of colors
+// if player wins the 32 different sequence then it diplays a "happy" sequence
+
 int main() {
   printf("Hello Pattern Game\n");
   return 0;
