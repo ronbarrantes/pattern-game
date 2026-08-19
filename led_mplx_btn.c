9d55b894a332e497356c6b85c926c13430997d6c
ronbarrantes <ronbarrantes@gmail.com>
2026-08-19 16:20:38 -0400

    Tweak the multiplexing

--- led_mplx_btn.c
+++ led_mplx_btn.c
@@ -3,6 +3,8 @@
 #include <stdint.h>
 #include <util/delay.h>
 
+#define STARTUP_DELAY 50
+
 /*
 DDRB   → Should this pin be INPUT or OUTPUT?
 PORTB  → Set output / enable input pull-up
@@ -10,47 +12,45 @@ PINB   → What is currently coming INTO the pin?
 */
 
 void startup_sequence(void) {
-  int delay_in_ms = 100;
   for (int i = 0; i < 3; i++) {
     // // LED ON
     PORTB |= (1 << PB0);
-    _delay_ms(delay_in_ms);
+    _delay_ms(STARTUP_DELAY);
 
     // // LED OFF
     PORTB &= ~(1 << PB0);
-    _delay_ms(delay_in_ms);
+    _delay_ms(STARTUP_DELAY);
   }
 }
 
 bool check_pressed(uint8_t pin) {
-  // check if button is pressed
-  DDRB &= ~(1 << pin);
-  PORTB |= (1 << pin);
-  _delay_ms(10);
+  DDRB &= ~(1 << pin); // input
+  PORTB |= (1 << pin); // pull-up
+  _delay_ms(5);
 
   return !(PINB & (1 << pin));
 }
 
-void set_led(uint8_t pin, bool is_pressed) {
-  DDRB |= (1 << pin);
-  if (is_pressed) {
+void set_led(uint8_t pin, bool state) {
+  if (state) {
     PORTB |= (1 << pin);
   } else {
     PORTB &= ~(1 << pin);
   }
+
+  DDRB |= (1 << pin); // output
 }
 
 int main(void) {
   // PB0 is output
   DDRB |= (1 << PB0);
-  DDRB &= ~(1 << PB1);
-  PORTB |= (1 << PB1);
-  bool is_pressed = 0;
+
+  bool is_pressed = false;
   uint8_t pb_arr[4] = {PB0, PB1, PB2, PB3};
 
   startup_sequence();
 
-  while (1) {
+  while (true) {
     for (uint8_t i = 0; i < 4; i++) {
       is_pressed = check_pressed(pb_arr[i]);
       set_led(pb_arr[i], is_pressed);
