2c5eeb9610b7fca9170eb8d60edee80e7ccc488e
ronbarrantes <ronbarrantes@gmail.com>
2026-08-18 23:45:35 -0400

    Do the led test

--- led_button.c
+++ led_button.c
@@ -2,7 +2,7 @@
 #include <util/delay.h>
 
 int main(void) {
-  // PB0 is input
+  // PB0 is output
   DDRB |= (1 << PB0);
 
   while (1) {
@@ -11,18 +11,8 @@ int main(void) {
     _delay_ms(500);
 
     // LED OFF
-    PORTB &= (1 << PB0);
+    PORTB &= ~(1 << PB0);
     _delay_ms(500);
   }
   return 0;
 }
-
-// PORTB = 10110100
-// mask  = 11111011
-//  ---------------
-//         10110000
-
-// PORTB = 10110100
-// mask  = 00000100
-//  ---------------
-//         00000100
