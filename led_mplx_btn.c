4c1598c8986b10c4fcce6138ba5d228edc1088cb
ronbarrantes <ronbarrantes@gmail.com>
2026-08-20 22:46:21 -0400

    tweak to the startup sequence

--- led_mplx_btn.c
+++ led_mplx_btn.c
@@ -3,51 +3,13 @@
 #include <stdint.h>
 #include <util/delay.h>
 
-#define STARTUP_DELAY 100
+#define STARTUP_DELAY 50
 
 /*
 DDRB   → Should this pin be INPUT or OUTPUT?
 PORTB  → Set output / enable input pull-up
 PINB   → What is currently coming INTO the pin?
 */
-
-void startup_sequence(uint8_t pin_arr[], uint8_t pb_length) {
-  for (int i = 0; i < pb_length; i++) {
-    // // LED ON
-    // pin sinks current
-    PORTB &= ~(1 << pin_arr[i]);
-    DDRB |= (1 << pin_arr[i]);
-    _delay_ms(STARTUP_DELAY * (i + 1));
-
-    // // LED OFF
-    // pin is an input with its pull-up enabled
-    DDRB &= ~(1 << pin_arr[i]);
-    PORTB |= (1 << pin_arr[i]);
-    _delay_ms(STARTUP_DELAY * (i + 1));
-  }
-
-  for (int i = pb_length - 1; i >= 0; i--) {
-    // // LED ON
-    // pin sinks current
-    PORTB &= ~(1 << pin_arr[i]);
-    DDRB |= (1 << pin_arr[i]);
-    _delay_ms(STARTUP_DELAY * (i + 1));
-
-    // // LED OFF
-    // pin is an input with its pull-up enabled
-    DDRB &= ~(1 << pin_arr[i]);
-    PORTB |= (1 << pin_arr[i]);
-    _delay_ms(STARTUP_DELAY * (i + 1));
-  }
-}
-
-bool check_pressed(uint8_t pin) {
-  DDRB &= ~(1 << pin); // input
-  PORTB |= (1 << pin); // pull-up
-  _delay_ms(5);
-
-  return !(PINB & (1 << pin));
-}
 
 void set_led(uint8_t pin, bool state) {
   if (state) {
@@ -59,6 +21,39 @@ void set_led(uint8_t pin, bool state) {
   }
 }
 
+void startup_sequence(uint8_t pin_arr[], uint8_t pb_length) {
+  for (int i = 0; i < pb_length; i++) {
+    // // LED ON
+    // pin sinks current
+    set_led(pin_arr[i], true);
+    _delay_ms(STARTUP_DELAY);
+
+    // // LED OFF
+    // pin is an input with its pull-up enabled
+    set_led(pin_arr[i], false);
+    _delay_ms(STARTUP_DELAY);
+  }
+
+  for (int i = pb_length; i >= 0; i--) {
+    // // LED ON
+    // pin sinks current
+    set_led(pin_arr[i], true);
+    _delay_ms(STARTUP_DELAY);
+
+    // // LED OFF
+    // pin is an input with its pull-up enabled
+    set_led(pin_arr[i], false);
+    _delay_ms(STARTUP_DELAY);
+  }
+}
+
+bool check_pressed(uint8_t pin) {
+  set_led(pin, false);
+  _delay_ms(5);
+
+  return !(PINB & (1 << pin));
+}
+
 int main(void) {
   bool is_pressed = false;
   uint8_t pb_arr[4] = {PB0, PB1, PB2, PB3};
@@ -66,22 +61,10 @@ int main(void) {
 
   // SET EVERYTHING TO OUTPUT
   for (int i = 0; i < pb_length; i++) {
-    DDRB |= (1 << pb_arr[i]);
+    set_led(pb_arr[i], false);
   }
 
-  // level 0 the lobby
-  // level 1 the havital zone parking garage
-  // level 2 is where thing start to get crippy
-  // //  level fun :)
-  // level 3 the abandoned hotel
-  // level 4 the pipe room
-  // // gold pipe room (secret)
-  // level 94 modern
-  // level 103 oi chips and vinegar
-  // level 900 the end
-  // level 999999999999 staairs
-  // level reality??
-
+  _delay_ms(100);
   startup_sequence(pb_arr, pb_length);
 
   while (true) {
