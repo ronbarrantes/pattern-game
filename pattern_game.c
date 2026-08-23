d60eda15189df5ad172e7cbc58e870ce77811d9d
Ron Barrantes <ronbarrantes@gmail.com>
2026-08-23 14:27:59 -0400

    Refactor LED control and game logic in pattern_game

--- pattern_game.c
+++ pattern_game.c
@@ -15,266 +15,267 @@
 #define BLUE_LED PB4
 
 typedef struct {
-  uint8_t led;
-  uint16_t duration;
+uint8_t led;
+uint16_t duration;
 } LightPattern;
 
 uint8_t pb_arr[] = {RED_LED, YELLOW_LED, GREEN_LED, BLUE_LED};
 
 
 void delay_ms(uint16_t duration_ms) {
-  while (duration_ms > 0) {
-    _delay_ms(1);
-    duration_ms--;
-  }
+while (duration_ms > 0) {
+_delay_ms(1);
+duration_ms--;
+}
 }
 
 LightPattern startup_pattern[] = {
-    {RED_LED, SHORT_DELAY},   {YELLOW_LED, SHORT_DELAY},
-    {GREEN_LED, SHORT_DELAY}, {BLUE_LED, SHORT_DELAY},
-    {GREEN_LED, SHORT_DELAY}, {YELLOW_LED, SHORT_DELAY},
-    {RED_LED, SHORT_DELAY},
+{RED_LED, SHORT_DELAY},   {YELLOW_LED, SHORT_DELAY},
+{GREEN_LED, SHORT_DELAY}, {BLUE_LED, SHORT_DELAY},
+{GREEN_LED, SHORT_DELAY}, {YELLOW_LED, SHORT_DELAY},
+{RED_LED, SHORT_DELAY},
 };
 
 LightPattern lose_pattern[] = {
-    {BLUE_LED, SHORT_DELAY},   {GREEN_LED, SHORT_DELAY},
-    {YELLOW_LED, SHORT_DELAY}, {RED_LED, MID_DELAY},
-    {RED_LED, MID_DELAY},      {RED_LED, MID_DELAY},
-    {RED_LED, MID_DELAY},
+{BLUE_LED, SHORT_DELAY},   {GREEN_LED, SHORT_DELAY},
+{YELLOW_LED, SHORT_DELAY}, {RED_LED, MID_DELAY},
+{RED_LED, MID_DELAY},      {RED_LED, MID_DELAY},
+{RED_LED, MID_DELAY},
 };
 
 LightPattern win_pattern[] = {
-    {RED_LED, SHORT_DELAY},   {YELLOW_LED, SHORT_DELAY},
-    {GREEN_LED, SHORT_DELAY}, {BLUE_LED, SHORT_DELAY},
-    {GREEN_LED, SHORT_DELAY}, {YELLOW_LED, SHORT_DELAY},
+{RED_LED, SHORT_DELAY},   {YELLOW_LED, SHORT_DELAY},
+{GREEN_LED, SHORT_DELAY}, {BLUE_LED, SHORT_DELAY},
+{GREEN_LED, SHORT_DELAY}, {YELLOW_LED, SHORT_DELAY},
 };
 
 void set_led(uint8_t pin, bool state) {
-  if (state) {
-    PORTB &= ~(1 << pin);
-    DDRB |= (1 << pin);
-  } else {
-    DDRB &= ~(1 << pin);
-    PORTB |= (1 << pin);
-  }
+if (state) {
+PORTB &= ~(1 << pin);
+DDRB |= (1 << pin);
+} else {
+DDRB &= ~(1 << pin);
+PORTB |= (1 << pin);
+}
 }
 
 bool check_pressed(uint8_t pin) {
-  set_led(pin, false);
-  delay_ms(5);
+set_led(pin, false);
+delay_ms(5);
 
-  return !(PINB & (1 << pin));
+return !(PINB & (1 << pin));
 }
 
 void startup_sequence(uint8_t pin_arr[], uint8_t pb_length) {
-  for (int i = 0; i < pb_length; i++) {
-    set_led(pin_arr[i], true);
-    delay_ms(SHORT_DELAY);
+for (int i = 0; i < pb_length; i++) {
+set_led(pin_arr[i], true);
+delay_ms(SHORT_DELAY);
 
-    set_led(pin_arr[i], false);
-    delay_ms(SHORT_DELAY);
-  }
+set_led(pin_arr[i], false);
+delay_ms(SHORT_DELAY);
+}
 
-  delay_ms(SHORT_DELAY * 3);
+delay_ms(SHORT_DELAY * 3);
 
-  for (int i = pb_length - 1; i >= 0; i--) {
-    set_led(pin_arr[i], true);
-    delay_ms(SHORT_DELAY);
+for (int i = pb_length - 1; i >= 0; i--) {
+set_led(pin_arr[i], true);
+delay_ms(SHORT_DELAY);
 
-    set_led(pin_arr[i], false);
-    delay_ms(SHORT_DELAY);
-  }
+set_led(pin_arr[i], false);
+delay_ms(SHORT_DELAY);
+}
 }
 
 void light_pattern(LightPattern pattern[], uint8_t pattern_length) {
-  for (int i = 0; i < pattern_length; i++) {
-    set_led(pattern[i].led, true);
-    delay_ms(pattern[i].duration);
+for (int i = 0 ; i < pattern_length; i++) {
+set_led(pattern[i].led, true);
+delay_ms(pattern[i].duration);
 
-    set_led(pattern[i].led, false);
-    delay_ms(pattern[i].duration);
-  }
+set_led(pattern[i].led, false);
+delay_ms(pattern[i].duration);
+}
 }
 
 void sequence_light(uint8_t pattern_list[], uint8_t curr_turn) {
-  for (int i = 0; i < curr_turn; i++) {
-    set_led(pattern_list[i], true);
-    delay_ms(LONG_DELAY);
+for (int i = 0; i < curr_turn; i++) {
+set_led(pattern_list[i], true);
+delay_ms(LONG_DELAY);
 
-    set_led(pattern_list[i], false);
-    delay_ms(LONG_DELAY);
-  }
+set_led(pattern_list[i], false);
+delay_ms(LONG_DELAY);
+}
 }
 
 void game_init(uint8_t game_length, uint8_t pattern_list[], uint8_t pb_arr[],
-               uint8_t pb_length) {
-  for (int l_item = 0; l_item < game_length; l_item++) {
-    uint8_t idx = rand() % pb_length;
-    pattern_list[l_item] = pb_arr[idx];
-  }
+uint8_t pb_length) {
+for (int l_item = 0; l_item < game_length; l_item++) {
+uint8_t idx = rand() % pb_length;
+pattern_list[l_item] = pb_arr[idx];
+}
 }
 
 bool pattern(uint8_t pattern_list[], uint8_t curr_turn, uint8_t pb_arr[],
-             uint8_t pb_length) {
-  uint8_t curr_guess;
+uint8_t pb_length) {
+uint8_t curr_guess;
 
-  sequence_light(pattern_list, curr_turn);
+sequence_light(pattern_list, curr_turn);
 
-  for (uint8_t idx = 0; idx < curr_turn; idx++) {
-    bool got_press = false;
+for (uint8_t idx = 0; idx < curr_turn; idx++) {
+bool got_press = false;
 
-    while (!got_press) {
-      for (uint8_t j = 0; j < pb_length; j++) {
-        if (check_pressed(pb_arr[j])) {
-          curr_guess = pb_arr[j];
-          got_press = true;
-          break;
-        }
-      }
-    }
+while (!got_press) {
+for (uint8_t j = 0; j < pb_length; j++) {
+if (check_pressed(pb_arr[j])) {
+curr_guess = pb_arr[j];
+got_press = true;
+break;
+}
+}
+}
 
-    while (check_pressed(curr_guess)) {
-      // wait for release
-    }
+while (check_pressed(curr_guess)) {
+// wait for release
+}
 
-    if (curr_guess != pattern_list[idx]) {
-      return false;
-    }
-  }
+if (curr_guess != pattern_list[idx]) {
+return false;
+}
+}
 
-  return true;
+return true;
 }
 
 uint8_t choose_level(uint8_t pb_arr[], uint8_t pb_length) {
 
 
-  for (uint8_t i = 0; i < pb_length; i++) {
-    set_led(pb_arr[i], true);
-  }
+for (uint8_t i = 0; i < pb_length; i++) {
+set_led(pb_arr[i], true);
+}
 
-  delay_ms(200);
+delay_ms(200);
 
-  for (uint8_t i = 0; i < pb_length; i++) {
-    set_led(pb_arr[i], false);
-  }
+for (uint8_t i = 0; i < pb_length; i++) {
+set_led(pb_arr[i], false);
+}
 
-  delay_ms(200);
+delay_ms(200);
 
-  for (uint8_t i = 0; i < pb_length; i++) {
-    set_led(pb_arr[i], true);
-  }
+for (uint8_t i = 0; i < pb_length; i++) {
+set_led(pb_arr[i], true);
+}
 
-  delay_ms(200);
+delay_ms(200);
 
-  for (uint8_t i = 0; i < pb_length; i++) {
-    set_led(pb_arr[i], false);
-  }
+for (uint8_t i = 0; i < pb_length; i++) {
+set_led(pb_arr[i], false);
+}
 
-  delay_ms(200);
+delay_ms(200);
 
-  // CHOOSING LEVEL
-  //
+// CHOOSING LEVEL
+//
 
-  uint8_t levels_arr[] = {5, 10, 15, 20};
-  uint8_t level;
-  
-  for (uint8_t idx = 0; idx < curr_turn; idx++) {
-    bool got_press = false;
+uint8_t levels_arr[] = {5, 10, 15, 20};
+uint8_t level;
+uint8_t selected_pin;
 
-    while (!got_press) {
-      for (uint8_t j = 0; j < pb_length; j++) {
-        if (check_pressed(pb_arr[j])) {
-          level = levels_arr[j];
-          got_press = true;
-          break;
-        }
-      }
-    }
+bool got_press = false;
 
-    while (check_pressed(curr_guess)) {
-      // wait for release
-    }
+while (!got_press) {
+for (uint8_t j = 0; j < pb_length; j++) {
+if (check_pressed(pb_arr[j])) {
+level = levels_arr[j];
+selected_pin = pb_arr[j];
+got_press = true;
+break;
+}
+}
+}
 
-    return level;
-  }
+while (check_pressed(selected_pin)) {
+// wait for release
+}
 
-//// MAIN FUNCTION 
-  int main(void) {
+return level;
+}
 
-    uint8_t game_length = 3;
-    uint8_t curr_turn = 1;
-    uint8_t pattern_list[game_length];
+//// MAIN FUNCTION
+int main(void) {
 
-    uint8_t pb_length = sizeof(pb_arr) / sizeof(pb_arr[0]);
+uint8_t game_length;
+uint8_t curr_turn = 1;
+uint8_t pattern_list[20];
 
-    uint8_t startup_pattern_length =
-        sizeof(startup_pattern) / sizeof(startup_pattern[0]);
+uint8_t pb_length = sizeof(pb_arr) / sizeof(pb_arr[0]);
 
-    uint8_t win_pattern_length = sizeof(win_pattern) / sizeof(win_pattern[0]);
+uint8_t startup_pattern_length =
+sizeof(startup_pattern) / sizeof(startup_pattern[0]);
 
-    uint8_t lose_pattern_length =
-        sizeof(lose_pattern) / sizeof(lose_pattern[0]);
+uint8_t win_pattern_length = sizeof(win_pattern) / sizeof(win_pattern[0]);
 
-    uint16_t seed = 0;
-    bool started = false;
+uint8_t lose_pattern_length =
+sizeof(lose_pattern) / sizeof(lose_pattern[0]);
 
-    for (int i = 0; i < pb_length; i++) {
-      set_led(pb_arr[i], false);
-    }
+uint16_t seed = 0;
+bool started = false;
 
-    _delay_ms(100);
+for (int i = 0; i < pb_length; i++) {
+set_led(pb_arr[i], false);
+}
 
-    light_pattern(startup_pattern, startup_pattern_length);
+_delay_ms(100);
 
-    /// Seeding the randomness
-    while (!started) {
-      seed++;
+light_pattern(startup_pattern, startup_pattern_length);
 
-      for (uint8_t i = 0; i < pb_length; i++) {
-        if (check_pressed(pb_arr[i])) {
-          started = true;
-          break;
-        }
-      }
-    }
-    srand(seed);
+/// Seeding the randomness
+while (!started) {
+seed++;
 
-    /// END: seeding
+for (uint8_t i = 0; i < pb_length; i++) {
+if (check_pressed(pb_arr[i])) {
+started = true;
+break;
+}
+}
+}
+srand(seed);
 
-    choose_level(pb_arr, pb_length);
+/// END: seeding
 
-    for (int i = 0; i < pb_length; i++) {
-    set_led(pb_arr[i], false);
-    }
+game_length = choose_level(pb_arr, pb_length);
 
-    delay_ms(2000);
+for (int i = 0; i < pb_length; i++) {
+set_led(pb_arr[i], false);
+}
 
-    game_init(game_length, pattern_list, pb_arr, pb_length);
-    delay_ms(2000);
+delay_ms(2000);
 
-    
-
+game_init(game_length, pattern_list, pb_arr, pb_length);
+delay_ms(2000);
 
 
 
-    for (int i = 0; i < pb_length; i++) {
-      set_led(pb_arr[i], false);
-    }
 
 
-    while (curr_turn <= game_length) {
-      bool is_pattern_correct =
-          pattern(pattern_list, curr_turn, pb_arr, pb_length);
 
-      if (!is_pattern_correct) {
-        light_pattern(lose_pattern, lose_pattern_length);
-        return 0;
-      }
+for (int i = 0; i < pb_length; i++) {
+set_led(pb_arr[i], false);
+}
 
-      curr_turn++;
-    }
 
-    light_pattern(win_pattern, win_pattern_length);
+while (curr_turn <= game_length) {
+bool is_pattern_correct =
+pattern(pattern_list, curr_turn, pb_arr, pb_length);
 
-    return 0;
-  }
+if (!is_pattern_correct) {
+light_pattern(lose_pattern, lose_pattern_length);
+return 0;
+}
+
+curr_turn++;
+}
+
+light_pattern(win_pattern, win_pattern_length);
+
+return 0;
+}
